b2ab87b2f1d6824aac812ed25c55ad6c529946ba
Khalis Khalil <khalistanya@gmail.com>
2026-09-02 02:55:23 +0300

    fix: remove edge runtime export from client components to fix client-side exceptions

--- app/questions/[id]/client-page.tsx
+++ app/questions/[id]/client-page.tsx
@@ -1,5 +1,4 @@
 'use client';
-export const runtime = 'edge';
 
 import { useQuery } from '@tanstack/react-query';
 import { useParams, useRouter } from 'next/navigation';
--- app/users/[id]/client-page.tsx
+++ app/users/[id]/client-page.tsx
@@ -1,5 +1,4 @@
 'use client';
-export const runtime = 'edge';
 
 import { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
